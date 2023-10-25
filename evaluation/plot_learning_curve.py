a633b50096a6e01ccbd465ece1f65330f32cbbb2
christoph14 <christoph.sandrock12@gmail.com>
2023-10-25 15:43:42 +0200

    Add data sets, small changes

--- evaluation/plot_learning_curve.py
+++ evaluation/plot_learning_curve.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-import sys
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -10,13 +9,13 @@ from utils.evaluation import load_results
 
 # Parse arguments
 parser = argparse.ArgumentParser(description='Plot learning curves.')
-parser.add_argument('--path', dest='path', type=str, default='results/',
+parser.add_argument('--path', dest='path', type=str, default='results',
                     help='the path to store the evaluation output files')
 parser.add_argument('--use_train_error', dest='use_train_error', action='store_const', const=True, default=False)
 args = parser.parse_args()
 
 plot_style = {
-    'rand': {'label': 'rand', 'color': '#858585', 'linestyle': '--'},
+    'rand': {'label': 'Rand', 'color': '#858585', 'linestyle': '--'},
     'lc': {'label': 'LC', 'color': '#6DB356', 'linestyle': '--'},
     'qbc': {'label': 'QBC', 'color': '#21780F', 'linestyle': '--'},
     'epis': {'label': 'Epis', 'color': '#BB31B7', 'linestyle': '--'},
@@ -58,7 +57,7 @@ algorithms['comparison'] = [
     'optimal_5_1_1000_ascending',
 ]
 
-datasets = [814, 1510, 54, 1462, 20, 36, 182]
+datasets = [814, 1510, 54, 1462, 20, 36, 182, 11, 37, 1063, 1464, 1494, 1504]
 for key, value in algorithms.items():
     results = load_results(args.path, datasets, value)
     for dataset_id in datasets:
@@ -66,7 +65,7 @@ for key, value in algorithms.items():
         fig, ax = plt.subplots(figsize=(2.24, 1.44))
         ylim_top = 0
         for i_qs, qs_name in enumerate(value):
-            if qs_name == 'epis' and dataset_id in [54, 20, 36, 182]:
+            if qs_name == 'epis' and dataset_id in [54, 20, 36, 182, 11]:
                 continue
             n_labels = np.loadtxt(args.path + f'/{dataset_id}/n_labels#{qs_name}#{0}.csv')
             error = np.empty((25, len(n_labels)))
